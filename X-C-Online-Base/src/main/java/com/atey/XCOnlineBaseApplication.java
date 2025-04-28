000e3aa9d78cf22a32635b98dfee353b02556e9b
zhf2680536614 <zhf19993612440@outlook.com>
2025-04-28 16:32:32 +0800

    初始化父工程和Base工程

---
+++ X-C-Online-Base/src/main/java/com/atey/XCOnlineBaseApplication.java
@@ -0,0 +1,13 @@
+package com.atey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XCOnlineBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XCOnlineBaseApplication.class, args);
+    }
+
+}
